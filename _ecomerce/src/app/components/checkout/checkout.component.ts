e61da1417d58971ad531c3a1c87c41b8c6cc6448
eyoeldefare <edefare@masonlive.gmu.edu>
2017-11-22 00:56:47 -0500

    designed some page documents

--- _ecomerce/src/app/components/checkout/checkout.component.ts
+++ _ecomerce/src/app/components/checkout/checkout.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CategoryAndProductsService } from '../../services/category-and-products.service';
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
+
 export class CheckoutComponent implements OnInit {
-  pro:any;
+  pro: any;
   constructor(private route: ActivatedRoute, private cps: CategoryAndProductsService) {
     this.getEachProducts();
   }
 
   ngOnInit() {
-    this.getEachProducts();
   }
 
   getEachProducts(): void {
     this.cps.getEachProduct(this.route.snapshot.params['id']).subscribe(data => {
+      setTimeout(function () {
+        $('.gif-container').css('display', 'none');
+      }, 1000);
       this.pro = data.product;
     });
   }
